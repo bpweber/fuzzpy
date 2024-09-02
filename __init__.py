9075d6fc5b753474fc7ea288a3904164ee03cb29
Bryce Weber <bpweber98@gmail.com>
2024-09-01 22:49:01 -0700

    update input handling

--- __init__.py
+++ __init__.py
@@ -19,31 +19,37 @@ def rate(str1, str2):
 if __name__ == '__main__':
     os.system('clear')
     last_path = ''
+    rlist = []
     while True:
-        path = os.getcwd()
-        search_t = input() 
-        os.system('clear')
-        if not search_t:
-            if os.path.isdir(last_path):
-                os.system('clear')
-                print(last_path)
-            else:
+        try:
+            path = os.getcwd()
+            search_t = input() 
+            os.system('clear')
+            if not search_t:
                 os.system(f'nvim {last_path}')
+                exit()
+            elif search_t.isdigit():
+                os.system(f'nvim {rlist[int(search_t)][1]}')
+                exit()
+            rlist = []
+            dlist = []
+            flist = []
+            for r, d, f in os.walk(path):
+                for n in f:
+                    flist.append(os.path.join(r, n))
+                for n in d:
+                    dlist.append(os.path.join(r, n))
+            for n in flist:
+                rlist.append([rate(n, search_t), n])
+            for n in dlist:
+                rlist.append([rate(n, search_t), n])
+            rlist = sorted(rlist, reverse=True)
+            for i in range(9, -1, -1):
+                if i == 0: 
+                    print(f'--> {rlist[i][1]}')
+                else:
+                    print(f'[{i}] {rlist[i][1]}')
+            last_path = rlist[0][1]
+        except KeyboardInterrupt:
             exit()
-        rlist = []
-        dlist = []
-        flist = []
-        for r, d, f in os.walk(path):
-            for n in f:
-                flist.append(os.path.join(r, n))
-            for n in d:
-                dlist.append(os.path.join(r, n))
-        for n in flist:
-            rlist.append([rate(n, search_t), n])
-        for n in dlist:
-            rlist.append([rate(n, search_t), n])
-        rlist = sorted(rlist, reverse=False)
-        for i in range(10, 0, -1):
-            print(rlist[-i])
-        last_path = rlist[-1][1]
 
